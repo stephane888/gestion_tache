762c07cd7a12c12361a21e3ef1cb64c37784f280
kouwa stephane <kksasteph888@gmail.com>
2024-10-07 15:04:42 +0200

    Maj

--- src/EventSubscriber/GestionProjectV2Subscriber.php
+++ src/EventSubscriber/GestionProjectV2Subscriber.php
@@ -1,25 +1,32 @@
-// <?php
+<?php
 
-// namespace Drupal\gestion_tache\EventSubscriber;
+namespace Drupal\gestion_tache\EventSubscriber;
 
-// use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-// use Drupal\jsonapi\ResourceType\ResourceTypeBuildEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\jsonapi\ResourceType\ResourceTypeBuildEvents;
+use Drupal\jsonapi\ResourceType\ResourceTypeBuildEvent;
 
-// /**
-//  *
-//  * @author stephane
-//  * @see https://www.drupal.org/docs/core-modules-and-themes/core-modules/jsonapi-module/customizing-resources
-//  */
-// class GestionProjectV2Subscriber implements EventSubscriberInterface {
+/**
+ *
+ * @author stephane
+ * @see https://www.drupal.org/docs/core-modules-and-themes/core-modules/jsonapi-module/customizing-resources
+ */
+class GestionProjectV2Subscriber implements EventSubscriberInterface {
   
-//   /**
-//    *
-//    * {@inheritdoc}
-//    */
-//   public static function getSubscribedEvents() {
-//     return [
-//       ResourceTypeBuildEvents::BUILD => []
-//     ];
-//   }
+  function onResourceTypeBuild(ResourceTypeBuildEvent $event) {
+    // add count meta datas.
+  }
   
-// }
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      ResourceTypeBuildEvents::BUILD => [
+        'onResourceTypeBuild'
+      ]
+    ];
+  }
+  
+}
